4abde25a630d743d72c0bb6c0bfb3790486951f0
nuclearcat87 <undernzty1@gmail.com>
2024-07-18 22:11:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function countVowelsAndConsonants() {
+    const text = document.getElementById('textInput').value;
+    const vowels = 'аеёиоуыэюяАЕЁИОУЫЭЮЯ';
+    const consonants = 'бвгджзйклмнпрстфхцчшщБВГДЖЗЙКЛМНПРСТФХЦЧШЩ';
+    let vowelCount = 0;
+    let consonantCount = 0;
+
+    for (let char of text) {
+        if (vowels.includes(char)) {
+            vowelCount++;
+        } else if (consonants.includes(char)) {
+            consonantCount++;
+        }
+    }
+
+    document.getElementById('result').innerText = 
+        `Гласные: ${vowelCount}, Согласные: ${consonantCount}`;
+}
